f319761843505e21218be7077373a8404025b3df
JUANITOss <0juansuazo0@gmail.com>
2024-10-01 16:47:35 -0300

    agregados archivos placeholder para definir jerarquia, creada conexion a back con axios

---
+++ client/src/assets/LandingPage.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import NavBar from "./NavBar";
+import Footer from "./Footer";
+export default function LandingPAge() {
+    return (
+        <div>
+            <NavBar/>
+            <main>
+                
+            </main>
+            <Footer/>
+        </div>
+    )
+}
