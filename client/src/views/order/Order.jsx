23fbad03fc13f2331935695a553f1fac17c2db5a
JUANITOss <0juansuazo0@gmail.com>
2024-10-20 20:27:04 -0300

    arregle masomenos product

--- client/src/views/order/Order.jsx
+++ client/src/views/order/Order.jsx
@@ -27,103 +27,103 @@ const Order = () => {
         setConvertingOrderId(null); // Reset the converting order state
     };
 
+    const userRole = localStorage.getItem('role'); // Get the user's role
+
     return (
-        <div className="flex flex-col min-h-screen bg-gray-900 p-6"> {/* Use flex and min-h-screen to fill the viewport */}
-            <div className="max-w-4xl mx-auto flex-grow"> {/* Allow this div to grow and take up available space */}
-                {/* Back to home arrow */}
-                <div className="flex items-center mb-4">
-                    <Link to="/" className="flex items-center text-green-400 hover:text-green-300 transition">
-                        <ArrowLeftIcon className="h-6 w-6 mr-2" /> {/* Arrow icon with some margin */}
-                        Back to home
-                    </Link>
+        <div className="flex flex-col min-h-screen bg-gray-900 p-6">
+            {/* Back to home arrow */}
+            <div className="flex items-center mb-4 mt-2 ml-2"> {/* Top left corner */}
+                <Link to="/" className="flex items-center text-green-400 hover:text-green-300 transition">
+                    <ArrowLeftIcon className="h-6 w-6 mr-2" />
+                    Back to home
+                </Link>
+            </div>
+
+            <h1 className="text-3xl font-bold mb-6 text-green-400 text-center">Your Orders</h1>
+
+            {/* Search Bar for Admins */}
+            {userRole === 'ADMIN' && (
+                <div className="mb-6 text-center">
+                    <input
+                        type="text"
+                        value={searchUserId}
+                        onChange={handleSearchChange}
+                        placeholder="Enter User ID"
+                        className="border rounded-md p-2 border-purple-600 bg-gray-700 text-green-300 focus:outline-none focus:ring-2 focus:ring-green-300 transition duration-200"
+                    />
+                    <button onClick={handleSearch} className="bg-purple-600 text-white px-4 py-2 rounded-md ml-2 transition duration-200 hover:bg-purple-500">
+                        Search Orders
+                    </button>
                 </div>
+            )}
 
-                <h1 className="text-3xl font-bold mb-6 text-green-400 text-center">Your Orders</h1>
-
-                {/* Search Bar for Admins */}
-                {localStorage.getItem('role') === 'ADMIN' && (
-                    <div className="mb-6 text-center">
-                        <input
-                            type="text"
-                            value={searchUserId}
-                            onChange={handleSearchChange}
-                            placeholder="Enter User ID"
-                            className="border rounded-md p-2 border-purple-600 bg-gray-700 text-green-300 focus:outline-none focus:ring-2 focus:ring-green-300 transition duration-200"
-                        />
-                        <button onClick={handleSearch} className="bg-purple-600 text-white px-4 py-2 rounded-md ml-2 transition duration-200 hover:bg-purple-500">
-                            Search Orders
-                        </button>
-                    </div>
-                )}
-
-                {/* Order List */}
-                <div className="flex flex-col items-center space-y-4">
-                    {orders.map(order => (
-                        <div key={order.id} className="border border-gray-700 p-6 rounded-lg bg-gray-800 shadow-md transition duration-200 hover:shadow-lg w-full max-w-2xl"> {/* Adjusted width */}
-                            <h2 className="font-bold text-purple-300 mb-2">Order ID: {order.id} | Date: {new Date(order.orderDate).toLocaleDateString()}</h2>
-                            
-                            <div className="mt-2">
-                                <h3 className="font-semibold text-green-300 mb-2">Products:</h3>
-                                <table className="table-auto w-full text-gray-400">
-                                    <thead>
-                                        <tr className="border-b border-gray-600">
-                                            <th className="px-4 py-2 text-left">Product Name</th>
-                                            <th className="px-4 py-2 text-left">Quantity</th>
-                                            <th className="px-4 py-2 text-left">Unit Price</th>
-                                            <th className="px-4 py-2 text-left">Total Price</th>
+            {/* Order List */}
+            <div className="flex flex-col items-center space-y-4 flex-grow">
+                {orders.map(order => (
+                    <div key={order.id} className="border border-gray-700 p-6 rounded-lg bg-gray-800 shadow-md transition duration-200 hover:shadow-lg w-full max-w-2xl"> {/* Adjusted width */}
+                        <h2 className="font-bold text-purple-300 mb-2">Order ID: {order.id} | Date: {new Date(order.orderDate).toLocaleDateString()}</h2>
+                        
+                        <div className="mt-2">
+                            <h3 className="font-semibold text-green-300 mb-2">Products:</h3>
+                            <table className="table-auto w-full text-gray-400">
+                                <thead>
+                                    <tr className="border-b border-gray-600">
+                                        <th className="px-4 py-2 text-left">Product Name</th>
+                                        <th className="px-4 py-2 text-left">Quantity</th>
+                                        <th className="px-4 py-2 text-left">Unit Price</th>
+                                        <th className="px-4 py-2 text-left">Total Price</th>
+                                    </tr>
+                                </thead>
+                                <tbody>
+                                    {order.orderProducts.map(product => (
+                                        <tr key={product.productId} className="border-b border-gray-600">
+                                            <td className="px-4 py-2">{product.productName}</td>
+                                            <td className="px-4 py-2">{product.quantity}</td>
+                                            <td className="px-4 py-2">${product.productPrice.toFixed(2)}</td>
+                                            <td className="px-4 py-2">${(product.productPrice * product.quantity).toFixed(2)}</td>
                                         </tr>
-                                    </thead>
-                                    <tbody>
-                                        {order.orderProducts.map(product => (
-                                            <tr key={product.productId} className="border-b border-gray-600">
-                                                <td className="px-4 py-2">{product.productName}</td>
-                                                <td className="px-4 py-2">{product.quantity}</td>
-                                                <td className="px-4 py-2">${product.productPrice.toFixed(2)}</td>
-                                                <td className="px-4 py-2">${(product.productPrice * product.quantity).toFixed(2)}</td>
-                                            </tr>
-                                        ))}
-                                    </tbody>
-                                </table>
-                            </div>
+                                    ))}
+                                </tbody>
+                            </table>
+                        </div>
 
+                        <div className="flex justify-center mt-4">
+                            <p className="font-bold text-purple-400 mb-4">Total Amount: ${order.totalAmount.toFixed(2)}</p>
+                        </div>
+
+                        {/* Conditionally render Convert to Bill component */}
+                        {!order.convertedToBill && (
                             <div className="flex justify-center mt-4">
-                                <p className="font-bold text-purple-400 mb-4">Total Amount: ${order.totalAmount.toFixed(2)}</p>
-                            </div>
-
-                            {/* Conditionally render Convert to Bill component */}
-                            {!order.convertedToBill && (
-                                <div className="flex justify-center mt-4">
-                                    {convertingOrderId === order.id ? (
-                                        <ConvertToBill orderId={order.id} onConvert={handleConversion} />
-                                    ) : (
-                                        <button
-                                            onClick={() => setConvertingOrderId(order.id)} // Set the order to convert
-                                            className="bg-green-600 text-white px-4 py-2 rounded-md transition duration-200 hover:bg-green-500"
-                                        >
-                                            Convert to Bill
-                                        </button>
-                                    )}
-                                </div>
-                            )}
-
-                            {/* Conditional rendering for delete button */}
-                            <div className="flex justify-center mt-4">
-                                {order.convertedToBill ? (
-                                    <p className="text-red-500">Sealed</p> 
+                                {convertingOrderId === order.id ? (
+                                    <ConvertToBill orderId={order.id} onConvert={handleConversion} />
                                 ) : (
                                     <button
-                                        onClick={() => deleteOrder(order.id)}
-                                        className="bg-red-600 text-white px-4 py-2 rounded-md transition duration-200 hover:bg-red-500"
+                                        onClick={() => setConvertingOrderId(order.id)} // Set the order to convert
+                                        className="bg-green-600 text-white px-4 py-2 rounded-md transition duration-200 hover:bg-green-500"
                                     >
-                                        Delete Order
+                                        Convert to Bill
                                     </button>
                                 )}
                             </div>
+                        )}
+
+                        {/* Conditional rendering for delete button */}
+                        <div className="flex justify-center mt-4">
+                            {order.convertedToBill ? (
+                                <p className="text-red-500">Sealed</p> 
+                            ) : (
+                                <button
+                                    onClick={() => deleteOrder(order.id)}
+                                    className="bg-red-600 text-white px-4 py-2 rounded-md transition duration-200 hover:bg-red-500"
+                                >
+                                    Delete Order
+                                </button>
+                            )}
                         </div>
-                    ))}
-                </div>
+                    </div>
+                ))}
             </div>
-            <Footer />
+            <Footer /> {/* Footer will always be at the bottom of the screen */}
         </div>
     );
 };
--- client/src/views/product/Product.jsx
+++ client/src/views/product/Product.jsx
@@ -1,6 +1,7 @@
-import React from 'react'; 
+import React, { useState } from 'react'; 
 import { useProductLogic } from '../../components/product/ProductLogic';
 import ProductForm from '../../components/product/ProductForm';
+import { Bars3Icon } from '@heroicons/react/24/outline';
 
 const Product = () => {
   const {
@@ -24,11 +25,13 @@ const Product = () => {
   } = useProductLogic();
 
   const placeholderImage = '/public/images/placeholder.png';
+  const userRole = localStorage.getItem('role');
+  const [filterMenuOpen, setFilterMenuOpen] = useState(false);
 
   return (
-    <div className="min-h-screen bg-gray-100 p-6">
+    <div className="min-h-screen bg-gray-900 p-6">
       <div className="max-w-4xl mx-auto">
-        <h1 className="text-4xl font-bold text-center text-gray-800 mb-8">Products</h1>
+        <h1 className="text-4xl font-bold text-center text-green-400 mb-8">Products</h1>
 
         {/* Search Bar */}
         <div className="flex items-center mb-8 space-x-4">
@@ -37,71 +40,87 @@ const Product = () => {
             placeholder="Search products by name"
             value={searchTerm}
             onChange={(e) => setSearchTerm(e.target.value)}
-            className="flex-grow px-4 py-2 border border-gray-300 rounded-md shadow-sm focus:ring focus:ring-indigo-200 focus:border-indigo-500"
+            className="flex-grow px-4 py-2 border border-gray-300 rounded-md shadow-sm focus:ring focus:ring-green-500 focus:border-green-500"
           />
           <button
             onClick={handleSearch}
-            className="px-4 py-2 bg-indigo-600 text-white font-semibold rounded-md hover:bg-indigo-500 transition"
+            className="px-4 py-2 bg-green-600 text-white font-semibold rounded-md hover:bg-green-500 transition"
           >
             Search
           </button>
         </div>
 
-        {/* Filter by Category */}
-        <div className="flex items-center mb-4">
-          <select
-            value={selectedCategory}
-            onChange={(e) => setSelectedCategory(e.target.value)}
-            className="px-4 py-2 border border-gray-300 rounded-md shadow-sm"
-          >
-            <option value="">Select Category</option>
-            {/* Agrega opciones de categorías aquí */}
-            <option value="1">Category 1</option>
-            <option value="2">Category 2</option>
-          </select>
-          <button
-            onClick={handleCategoryFilter}
-            className="ml-4 px-4 py-2 bg-blue-600 text-white font-semibold rounded-md hover:bg-blue-500 transition"
-          >
-            Filter by Category
-          </button>
-        </div>
-
-        {/* Filter by Price */}
-        <div className="flex items-center mb-4">
-          <input
-            type="number"
-            placeholder="Min Price"
-            value={minPrice}
-            onChange={(e) => setMinPrice(e.target.value)}
-            className="flex-grow px-4 py-2 border border-gray-300 rounded-md shadow-sm"
-          />
-          <input
-            type="number"
-            placeholder="Max Price"
-            value={maxPrice}
-            onChange={(e) => setMaxPrice(e.target.value)}
-            className="flex-grow px-4 py-2 border border-gray-300 rounded-md shadow-sm ml-2"
-          />
-          <button
-            onClick={handlePriceFilter}
-            className="ml-4 px-4 py-2 bg-green-600 text-white font-semibold rounded-md hover:bg-green-500 transition"
-          >
-            Filter by Price
-          </button>
-        </div>
-
-        {/* Create Product Button */}
-        <button
-          onClick={handleCreate}
-          className="mb-6 px-4 py-2 bg-green-600 text-white font-semibold rounded-md hover:bg-green-500 transition"
+        {/* Filter Menu */}
+        <button 
+          onClick={() => setFilterMenuOpen(!filterMenuOpen)} 
+          className="flex items-center mb-4 text-green-400 hover:text-green-300 transition"
         >
-          Create Product
+          <Bars3Icon className="h-6 w-6 mr-2" />
+          Filter
         </button>
+
+        {filterMenuOpen && (
+          <div className="bg-gray-800 p-4 rounded-md shadow-md mb-6">
+            <h2 className="text-lg font-bold text-green-400 mb-2">Filter Options</h2>
+            <div className="flex flex-col space-y-4">
+              <div className="flex items-center">
+                <select
+                  value={selectedCategory}
+                  onChange={(e) => setSelectedCategory(e.target.value)}
+                  className="px-4 py-2 border border-gray-300 rounded-md shadow-sm bg-gray-700 text-green-400"
+                >
+                  <option value="">Select Category</option>
+                  {/* Add category options here */}
+                  <option value="1">Category 1</option>
+                  <option value="2">Category 2</option>
+                </select>
+                <button
+                  onClick={handleCategoryFilter}
+                  className="ml-4 px-4 py-2 bg-green-600 text-white font-semibold rounded-md hover:bg-green-500 transition"
+                >
+                  Filter by Category
+                </button>
+              </div>
+
+              <div className="flex items-center">
+                <input
+                  type="number"
+                  placeholder="Min Price"
+                  value={minPrice}
+                  onChange={(e) => setMinPrice(e.target.value)}
+                  className="flex-grow px-4 py-2 border border-gray-300 rounded-md shadow-sm bg-gray-700 text-green-400"
+                />
+                <input
+                  type="number"
+                  placeholder="Max Price"
+                  value={maxPrice}
+                  onChange={(e) => setMaxPrice(e.target.value)}
+                  className="flex-grow px-4 py-2 border border-gray-300 rounded-md shadow-sm bg-gray-700 text-green-400 ml-2"
+                />
+                <button
+                  onClick={handlePriceFilter}
+                  className="ml-4 px-4 py-2 bg-green-600 text-white font-semibold rounded-md hover:bg-green-500 transition"
+                >
+                  Filter by Price
+                </button>
+              </div>
+            </div>
+          </div>
+        )}
+
+        {/* Create Product Button (Admin Only) */}
+        {userRole === 'ADMIN' && (
+          <button
+            onClick={handleCreate}
+            className="mb-6 px-4 py-2 bg-green-600 text-white font-semibold rounded-md hover:bg-green-500 transition"
+          >
+            Create Product
+          </button>
+        )}
 
         {/* Product Form (for both create and edit) */}
         {editingProduct && (
-          <div className="mb-8 p-4 bg-white shadow-lg rounded-md">
+          <div className="mb-8 p-4 bg-gray-800 shadow-lg rounded-md">
             <ProductForm product={editingProduct} onSave={handleSave} />
           </div>
         )}
@@ -109,12 +128,12 @@ const Product = () => {
         {/* Products List */}
         <ul className="space-y-6">
           {products.map((product) => (
-            <li key={product.id} className="p-6 bg-white rounded-lg shadow-md flex flex-col md:flex-row items-center justify-between">
+            <li key={product.id} className="p-6 bg-gray-800 rounded-lg shadow-md flex flex-col md:flex-row items-center justify-between">
               <div className="flex flex-col space-y-2">
-                <h3 className="text-2xl font-bold text-gray-700">{product.name}</h3>
-                <p className="text-gray-500">{product.description}</p>
-                <p className="text-gray-700 font-semibold">Price: ${product.price}</p>
-                <p className="text-gray-700">Stock: {product.stockQuantity}</p>
+                <h3 className="text-2xl font-bold text-green-400">{product.name}</h3>
+                <p className="text-gray-300">{product.description}</p>
+                <p className="text-gray-400 font-semibold">Price: ${product.price}</p>
+                <p className="text-gray-400">Stock: {product.stockQuantity}</p>
                 <p className="text-gray-500 text-sm">Category ID: {product.categoryId}</p>
 
                 <img
@@ -125,18 +144,40 @@ const Product = () => {
               </div>
 
               <div className="mt-4 md:mt-0 space-x-4 flex">
-                <button
-                  onClick={() => handleEdit(product)}
-                  className="px-4 py-2 bg-yellow-500 text-white rounded-md hover:bg-yellow-400 transition"
-                >
-                  Edit
-                </button>
-                <button
-                  onClick={() => handleDelete(product.id)}
-                  className="px-4 py-2 bg-red-600 text-white rounded-md hover:bg-red-500 transition"
-                >
-                  Delete
-                </button>
+                {/* Edit and Delete Buttons (Admin Only) */}
+                {userRole === 'ADMIN' && (
+                  <>
+                    <button
+                      onClick={() => handleEdit(product)}
+                      className="px-4 py-2 bg-yellow-500 text-white rounded-md hover:bg-yellow-400 transition"
+                    >
+                      Edit
+                    </button>
+                    <button
+                      onClick={() => handleDelete(product.id)}
+                      className="px-4 py-2 bg-red-600 text-white rounded-md hover:bg-red-500 transition"
+                    >
+                      Delete
+                    </button>
+                  </>
+                )}
+
+                {/* Add to Cart (User Only) */}
+                {userRole === 'USER' && (
+                  <div className="flex items-center">
+                    <input
+                      type="number"
+                      min="1"
+                      placeholder="Qty"
+                      className="w-16 p-2 border border-gray-300 rounded-md shadow-sm mr-2"
+                    />
+                    <button
+                      className="px-4 py-2 bg-violet-600 text-white rounded-md hover:bg-blue-500 transition"
+                    >
+                      Add to Cart
+                    </button>
+                  </div>
+                )}
               </div>
             </li>
           ))}
