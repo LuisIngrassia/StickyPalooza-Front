f5cc4eb8a0587300a69d0a4d4abb07a57fb1c5f7
lui <ingrassialuispedro@gmail.com>
2024-10-22 14:33:34 -0300

    cart view change

--- client/src/views/cart/Cart.jsx
+++ client/src/views/cart/Cart.jsx
@@ -1,8 +1,13 @@
-import React from 'react';
-import { useCartLogic } from '../../components/cart/CartLogic';
-import ConvertToOrder from '../../components/order/ConvertToOrder';
-import { useNavigate } from 'react-router-dom'; 
-import { ArrowLeftIcon, PlusIcon, MinusIcon, TrashIcon } from '@heroicons/react/24/solid'; // Importing the icons
+import React from "react";
+import { useCartLogic } from "../../components/cart/CartLogic";
+import ConvertToOrder from "../../components/order/ConvertToOrder";
+import { useNavigate } from "react-router-dom";
+import {
+  ArrowLeftIcon,
+  PlusIcon,
+  MinusIcon,
+  TrashIcon,
+} from "@heroicons/react/24/solid"; // Importing the icons
 import Footer from "../../components/general/Footer";
 
 const Cart = () => {
@@ -35,105 +40,129 @@ const Cart = () => {
   }
 
   return (
-    <div className="min-h-screen bg-gray-900 p-6 text-white flex flex-col items-center justify-center"> 
-
+    <div className="min-h-screen bg-gray-900 flex flex-col">
       <button
         className="absolute top-4 left-4 flex items-center text-green-400 hover:text-green-300 transition"
-        onClick={() => navigate('/')}
+        onClick={() => navigate("/")}
       >
         <ArrowLeftIcon className="h-6 w-6 mr-2" />
         Back to Home
       </button>
-      
-      <h2 className="text-3xl font-bold text-center text-green-400 mb-6">Your Cart</h2>
 
-      <div className="max-w-4xl w-full bg-gray-800 rounded-lg shadow-lg p-6 mb-6"> 
-        
-        {error && <div className="text-red-500 text-center mb-4">{error}</div>}
-        
-        {cart && cart.cartProducts && cart.cartProducts.length > 0 ? (
-          <>
-            <table className="min-w-full divide-y divide-gray-600">
-              <thead className="bg-gray-700">
-                <tr>
-                  <th className="px-4 py-2 text-left">Product</th>
-                  <th className="px-4 py-2 text-left">Price</th>
-                  <th className="px-4 py-2 text-left">Quantity</th>
-                  <th className="px-4 py-2 text-left">Total</th>
-                  <th className="px-4 py-2 text-left">Actions</th>
-                </tr>
-              </thead>
-              <tbody className="bg-gray-800 divide-y divide-gray-700">
-                {cart.cartProducts.map(product => {
-                  const productImage = product.productImage ? `http://localhost:5000${product.productImage}` : '/images/placeholder.png'; // Updated image logic
+      <main className="flex-grow flex flex-col items-center justify-center p-6">
+        <h2 className="text-3xl font-bold text-center text-green-400 mb-6">
+          Your Cart
+        </h2>
 
-                  return (
-                    <tr key={product.productId}>
-                      <td className="px-4 py-2 flex items-center">
-                        <img src={productImage} alt={product.productName} className="mt-4 mr-20 w-24 h-24 object-cover rounded-md mr-2" />
-                        <span className="font-bold text-green-400 text-xl">{product.productName}</span>
-                      </td>
-                      <td className="px-4 py-2">${product.productPrice.toFixed(2)}</td>
-                      <td className="px-4 py-2">{product.quantity}</td>
-                      <td className="px-4 py-2">${(product.productPrice * product.quantity).toFixed(2)}</td>
-                      <td className="px-4 py-2 flex-col items-center ">
-                        <div className="flex space-x-2 ">
+        <div className="max-w-4xl w-full bg-gray-800 rounded-lg shadow-lg p-6 mb-6">
+          {error && (
+            <div className="text-red-500 text-center mb-4">{error}</div>
+          )}
+
+          {cart && cart.cartProducts && cart.cartProducts.length > 0 ? (
+            <>
+              <table className="min-w-full divide-y divide-gray-600">
+                <thead className="bg-gray-700">
+                  <tr>
+                    <th className="px-4 py-2 text-left">Product</th>
+                    <th className="px-4 py-2 text-left">Price</th>
+                    <th className="px-4 py-2 text-left">Quantity</th>
+                    <th className="px-4 py-2 text-left">Total</th>
+                    <th className="px-4 py-2 text-left">Actions</th>
+                  </tr>
+                </thead>
+                <tbody className="bg-gray-800 divide-y divide-gray-700">
+                  {cart.cartProducts.map((product) => {
+                    const productImage = product.productImage
+                      ? `http://localhost:5000${product.productImage}`
+                      : "/images/placeholder.png"; // Updated image logic
+
+                    return (
+                      <tr key={product.productId}>
+                        <td className="px-4 py-2 flex items-center">
+                          <img
+                            src={productImage}
+                            alt={product.productName}
+                            className="mt-4 mr-20 w-24 h-24 object-cover rounded-md mr-2"
+                          />
+                          <span className="font-bold text-green-400 text-xl">
+                            {product.productName}
+                          </span>
+                        </td>
+                        <td className="px-4 py-2">
+                          ${product.productPrice.toFixed(2)}
+                        </td>
+                        <td className="px-4 py-2">{product.quantity}</td>
+                        <td className="px-4 py-2">
+                          $
+                          {(product.productPrice * product.quantity).toFixed(2)}
+                        </td>
+                        <td className="px-4 py-2 flex-col items-center ">
+                          <div className="flex space-x-2 ">
+                            <button
+                              onClick={() =>
+                                handleAddToCart(product.productId, 1)
+                              }
+                              className="bg-green-600 text-white rounded-md p-1 hover:bg-green-500 transition duration-200"
+                            >
+                              <PlusIcon className="h-6 w-6" />
+                            </button>
+                            <button
+                              onClick={() =>
+                                handleAddToCart(product.productId, -1)
+                              }
+                              className="bg-orange-600 text-white rounded-md p-1 hover:bg-orange-500 transition duration-200"
+                            >
+                              <MinusIcon className="h-6 w-6" />
+                            </button>
+                          </div>
                           <button
-                            onClick={() => handleAddToCart(product.productId, 1)}
-                            className="bg-green-600 text-white rounded-md p-1 hover:bg-green-500 transition duration-200"
+                            onClick={() =>
+                              handleRemoveFromCart(product.productId)
+                            }
+                            className="mt-2 bg-purple-600 text-white rounded-md p-1 hover:bg-purple-500 transition duration-200"
                           >
-                            <PlusIcon className="h-6 w-6" />
+                            <TrashIcon className="h-6 w-16" />
                           </button>
-                          <button
-                            onClick={() => handleAddToCart(product.productId, -1)}
-                            className="bg-orange-600 text-white rounded-md p-1 hover:bg-orange-500 transition duration-200"
-                          >
-                            <MinusIcon className="h-6 w-6" />
-                          </button>
-                        </div>
-                        <button
-                          onClick={() => handleRemoveFromCart(product.productId)}
-                          className="mt-2 bg-purple-600 text-white rounded-md p-1 hover:bg-purple-500 transition duration-200"
-                        >
-                          <TrashIcon className="h-6 w-16" />
-                        </button>
-                      </td>
-                    </tr>
-                  );
-                })}
-              </tbody>
-            </table>
+                        </td>
+                      </tr>
+                    );
+                  })}
+                </tbody>
+              </table>
 
-            <h3 className="text-xl font-bold text-center text-green-400 mt-6">
-              Total Cost: ${calculateTotalCost().toFixed(2)}
-            </h3>
+              <h3 className="text-xl font-bold text-center text-green-400 mt-6">
+                Total Cost: ${calculateTotalCost().toFixed(2)}
+              </h3>
 
-            <div className="mt-4 flex justify-center">
-              <ConvertToOrder
-                cartId={cart.id} 
-                onConvert={(order) => {
-                  console.log('Order converted:', order);
-                }}
-                className="w-1/2 h-10 bg-red-600 text-white rounded-md hover:bg-red-500 transition duration-200" 
-              >
-                Convert Cart To Order
-              </ConvertToOrder>
+              <div className="mt-4 flex justify-center">
+                <ConvertToOrder
+                  cartId={cart.id}
+                  onConvert={(order) => {
+                    console.log("Order converted:", order);
+                  }}
+                  className="w-1/2 h-10 bg-red-600 text-white rounded-md hover:bg-red-500 transition duration-200"
+                >
+                  Convert Cart To Order
+                </ConvertToOrder>
+              </div>
+
+              <div className="flex justify-center mt-4">
+                <button
+                  onClick={handleDeleteCartClick}
+                  className="w-1/2 h-10 bg-red-600 text-white rounded-md hover:bg-red-500 transition duration-200"
+                >
+                  Delete Entire Cart
+                </button>
+              </div>
+            </>
+          ) : (
+            <div className="text-center text-gray-400">
+              No items in your cart.
             </div>
-
-            <div className="flex justify-center mt-4">
-              <button
-                onClick={handleDeleteCartClick}
-                className="w-1/2 h-10 bg-red-600 text-white rounded-md hover:bg-red-500 transition duration-200"
-              >
-                Delete Entire Cart
-              </button>
-            </div>
-          </>
-        ) : (
-          <div className="text-center text-gray-400">No items in your cart.</div>
-        )}
-      </div>
-
+          )}
+        </div>
+      </main>
       <Footer />
     </div>
   );
